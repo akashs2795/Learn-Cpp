367adad6aae6725fe694fe5ac9f54f93acee1892
SumaKolamala <34037190+SumaKolamala@users.noreply.github.com>
2017-12-10 23:38:06 +0530

    Adding Swap and Sorting Algoritms (#2)
    * Added Sorting Algorithms
    
    Added Bubble, Selection and Insertion Sort.
    
    * Swap Functions
    
    Swap using call by reference and pointers
    
    * Edit
    
    Bubble Sort and Swap were edited
    
    * Edits to Sorting Algorithms and Swap
    
    Codes use dynamic arrays and are commented
    
    * Edit Swap

---
+++ Swap.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+using namespace std;
+
+void SwapRef(int &a, int &b) //to swap using call by reference
+{
+   int temp = a; 
+   a = b;    
+   b = temp;  
+}
+
+void SwapPoint(int *a, int *b) //to swap using pointters
+{
+   int temp = *a;   
+   *a = *b;     
+   *b = temp;   
+}
+
+void Print(int A[],int n) //to print the array
+{
+	cout<<"The array is ";
+	for (int i=0;i<n;i++)
+	{
+		cout<<A[i]<<" ";
+	}
+	cout<<endl;
+}
+
+int SwapWithArray (int A[],int index1, int index2) //to swap indices of an array by passing it in the function
+{
+	int temp;   
+	temp=*(A+index1);
+	*(A+index1)=*(A+index2);
+	*(A+index2)=temp;
+}
+
+
+int main ()
+{
+	int index1, index2, option, size, i;	
+	cout<<"Enter the number of elements"<<endl;
+	cin>>size;
+	int *A = new int[size];
+	cout<<"Enter the elements"<<endl;
+	for (i=0;i<size;i++)
+	  cin>>A[i];
+	cout<<"Enter the indices to swap"<<endl;
+	cin>>index1>>index2;
+	cout<<"If you want to swap using call by reference, press 1. If you want to use pointers, press 2. To swap by passing the array in the function, press 3"<<endl;
+	cin>>option;
+	switch (option)
+	{
+		case 1: Print (A, size);
+			SwapRef(A[index1], A[index2]); 
+			Print (A, size);
+			break;
+		case 2: Print (A, size);
+			SwapPoint(&A[index1], &A[index2]); 
+			Print (A, size);
+			break;
+		case 3: Print (A, size);
+			SwapWithArray(A,index1,index2);
+			Print (A, size);
+			break;
+
+	}
+}
+
+
